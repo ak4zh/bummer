5a0cbb444d6a39aa8a1dcb6cce38a3531b45198e
Ak4zh <agwl.akash@gmail.com>
2022-07-09 15:40:17 +0530

    updated db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,65 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { readable } from 'svelte/store';
+import { goto } from '$app/navigation';
+import { loading } from './utils/stores';
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+const dbAPI = async (query) => {
+	return await query;
+};
 
-export default supabase
+const Loader = (fn) => {
+	return async function (query) {
+		loading.set(true);
+		const { data, error } = await query;
+		loading.set(false);
+		return { data, error };
+	};
+};
+
+const AuthLoader = (fn) => {
+	return async function (query) {
+		loading.set(true);
+		const { error, session } = await query;
+		loading.set(false);
+		return { error, session };
+	};
+};
+
+const loaderQuery = Loader(dbAPI);
+const authLoaderQuery = AuthLoader(dbAPI);
+
+export const supabase = createClient(
+	String(import.meta.env.VITE_SUPABASE_URL),
+	String(import.meta.env.VITE_SUPABASE_ANON_KEY)
+);
+
+export const user = readable(supabase.auth.user(), (set) => {
+	supabase.auth.onAuthStateChange((event, session) => {
+		if (event == 'SIGNED_OUT') {
+			set(null);
+			goto('/');
+		} else if (event == 'SIGNED_IN') {
+			set(session.user);
+		}
+	});
+});
+
+export async function signOut() {
+	await supabase.auth.signOut();
+}
+
+export const auth = {
+	async signIn(data: object) {
+		const query = supabase.auth.signIn(data);
+		return await authLoaderQuery(query);
+	},
+
+	async signUp(data: object) {
+		const query = supabase.auth.signUp(data);
+		return await authLoaderQuery(query);
+	},
+
+	async signOut() {
+		return await supabase.auth.signOut();
+	}
+};
